9a2d494f88ff1ee37d9fe71520bac52a6d79e25d
szympie <justpiech@gmail.com>
2017-05-01 18:02:22 +0200

    fix

--- jsonFUNFUNFUN/classifier.py
+++ jsonFUNFUNFUN/classifier.py
@@ -2,19 +2,18 @@ import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import train_test_split
 
 from jsonFUNFUNFUN.createFeatures import create_features
 from loadJson import load_data
-from paths import trainingSetJSON100k,testSetJSON100K,testSetLabelsJSON100K
+from paths import trainingSetJSON100k,testSetJSON100K,testSetLabelsJSON100K, deprecated_test_labels_path, original_deprecated_testpaths
 
-# numberOfPackages = "ALL"
-numberOfPackages = 1
-X = create_features(load_data(trainingSetJSON100k,numberOfPackages),"list")
+
+X = create_features(load_data(trainingSetJSON100k),"list")
 X_train = X[:, 0:35]
 y_train = X[:, 35]
 
-X_test = create_features(load_data(testSetJSON100K,numberOfPackages),"list",'test')
-y_test = np.loadtxt(testSetLabelsJSON100K)
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
 
 scaler = Normalizer(copy=False)
 clf = MLPClassifier(hidden_layer_sizes=(7,3), verbose=True)
