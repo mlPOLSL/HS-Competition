c9af93e59d6b60a05b72e3cdb96da3b6a001c831
szympie <justpiech@gmail.com>
2017-05-01 17:40:06 +0200

    hot fix

--- jsonFUNFUNFUN/classifier.py
+++ jsonFUNFUNFUN/classifier.py
@@ -6,11 +6,14 @@ from sklearn.preprocessing import Normalizer
 from jsonFUNFUNFUN.createFeatures import create_features
 from loadJson import load_data
 from paths import trainingSetJSON100k,testSetJSON100K,testSetLabelsJSON100K
-X = create_features(load_data(trainingSetJSON100k),"list")
+
+# numberOfPackages = "ALL"
+numberOfPackages = 1
+X = create_features(load_data(trainingSetJSON100k,numberOfPackages),"list")
 X_train = X[:, 0:35]
 y_train = X[:, 35]
 
-X_test = create_features(load_data(testSetJSON100K),"list",'test')
+X_test = create_features(load_data(testSetJSON100K,numberOfPackages),"list",'test')
 y_test = np.loadtxt(testSetLabelsJSON100K)
 
 scaler = Normalizer(copy=False)
